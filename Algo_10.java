4d6863e68e392c50c7ded1b041cf4bb96ebf7db4
Nuwan Vidanapathirnage <nuwanpvp@gmail.com>
2023-08-13 22:57:35 +0530

    All  are completed.

--- Algo_10.java
+++ Algo_10.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Algo_10 {
-    
+
     private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         
